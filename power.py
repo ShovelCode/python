c1ec4821ef3bce3dea6164c422ee573ac6dbf94c
ShovelCode <rchss@rocketmail.com>
2020-11-02 02:05:54 -0600

    Update power.py

--- power.py
+++ power.py
@@ -1,1 +1,16 @@
 print(9 ** 2)
+list1 = [x ** 2 for x in range(10)]
+print(list1)
+
+list2 = [x + y for x in 'spam' for y in 'SPAM']
+
+print(list2)
+
+list3 = []
+for x in range(10):
+    if x % 2 == 0:
+        for y in range(10):
+            if y % 2 == 1:
+                list3.append((x,y))
+
+print(list3)
