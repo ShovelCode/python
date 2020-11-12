8b71ac2158d9a71e8afe21692cdfdc9b8b3febb8
ShovelCode <vde435@gmail.com>
2020-11-12 03:06:57 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -8,8 +8,11 @@ and del global not with
 as elif if or yield 
 assert else import pass
 break except in raise '''
+import math
 
 reserved_words = set(['False', 'class', 'finally', 'is', 'return', 'None', 'continue', 'for', 'lambda', 'try', 'True', 'def', 'from', 'nonlocal', 'while', 'and', 'del', 'global', 'not', 'with', 'as', 'elif', 'if', 'or', 'yield', 'assert', 'else', 'import', 'pass', 'break', 'except', 'in', 'raise'])
-
+reserved_words_check = 'until' in reserved_words #not Perl
+four_squares = [x * 2 for x in [abs(x) for x in (-1,-2,3,4)]]
 #keep only one print statement
-print('until' in reserved_words)
+print(four_squares)
+
