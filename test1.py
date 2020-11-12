8e342e8fc52808d30b1158d12cddd73e05b21ac0
ShovelCode <vde435@gmail.com>
2020-11-12 02:51:34 -0600

    Create test1.py

---
+++ test1.py
@@ -0,0 +1,15 @@
+''' 
+big block comment
+Python 3.x reserved words
+False class finally is return
+None continue for lambda try
+True def from nonlocal while
+and del global not with
+as elif if or yield 
+assert else import pass
+break except in raise '''
+
+reserved_words = set(['False', 'class', 'finally', 'is', 'return', 'None', 'continue', 'for', 'lambda', 'try', 'True', 'def', 'from', 'nonlocal', 'while', 'and', 'del', 'global', 'not', 'with', 'as', 'elif', 'if', 'or', 'yield', 'assert', 'else', 'import', 'pass', 'break', 'except', 'in', 'raise'])
+
+#keep only one print statement
+print('until' in reserved_words)
