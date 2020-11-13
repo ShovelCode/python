3ddfac9e650228ffb9895fa1164f079bf35ec5d6
ShovelCode <vde435@gmail.com>
2020-11-12 21:02:32 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,9 +10,16 @@ assert else import pass
 break except in raise '''
 import math
 
+def scramble(seq):
+    res = []
+    for i in range(len(seq)):
+        res.append(seq[i:] + seq[:i])
+    return res
+
 reserved_words = set(['False', 'class', 'finally', 'is', 'return', 'None', 'continue', 'for', 'lambda', 'try', 'True', 'def', 'from', 'nonlocal', 'while', 'and', 'del', 'global', 'not', 'with', 'as', 'elif', 'if', 'or', 'yield', 'assert', 'else', 'import', 'pass', 'break', 'except', 'in', 'raise'])
 reserved_words_check = 'until' in reserved_words #not Perl
 four_squares = [x * 2 for x in [abs(x) for x in (-1,-2,3,4)]]
+scrambled_eggs = scramble('eggs')
 #keep only one print statement
-print('test string')
+print(scrambled_eggs)
 
