da297703f9c4d7410adec51a9ff50febfcbc255d
shovelcode <vde435@gmail.com>
2020-11-13 13:44:55 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -9,6 +9,7 @@ as elif if or yield
 assert else import pass
 break except in raise '''
 import math
+import json #not used
 
 reserved_words = set(['False', 'class', 'finally', 'is', 'return', 'None', 'continue', 'for', 'lambda', 'try', 'True', 'def', 'from', 'nonlocal', 'while', 'and', 'del', 'global', 'not', 'with', 'as', 'elif', 'if', 'or', 'yield', 'assert', 'else', 'import', 'pass', 'break', 'except', 'in', 'raise'])
 reserved_words_check = 'until' in reserved_words #not Perl
