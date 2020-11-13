d455660679f50d4c89e48aefbb638a13825a1019
ShovelCode <vde435@gmail.com>
2020-11-12 18:07:13 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -14,5 +14,5 @@ reserved_words = set(['False', 'class', 'finally', 'is', 'return', 'None', 'cont
 reserved_words_check = 'until' in reserved_words #not Perl
 four_squares = [x * 2 for x in [abs(x) for x in (-1,-2,3,4)]]
 #keep only one print statement
-print(four_squares)
+print('test string')
 
